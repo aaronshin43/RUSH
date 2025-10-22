be334d8542fd90b653787a61737891dda6d49aa4
aaronshin43 <aaronshin.exe@gmail.com>
2025-10-22 12:11:09 -0400

    Change path of .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     
     # Pydantic V2 설정
     model_config = SettingsConfigDict(
-        env_file="../.env",
+        env_file=".env",
         case_sensitive=True,
         extra="ignore"
     )
